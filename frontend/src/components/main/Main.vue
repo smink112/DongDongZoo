e71b3a8b4c1ebb74d20252b7ba0061d716d5d430
박현우 <gogoadl@naver.com>
2024-03-11 17:33:31 +0900

    Feat : 로그인 페이지 UI 추가

--- frontend/src/components/main/Main.vue
+++ frontend/src/components/main/Main.vue
@@ -119,17 +119,4 @@ onUnmounted(() => {
   margin-left: 100px;
   margin-top: 300px;
 }
-.slide-fade-enter-active {
-  transition: all 0.8s ease-out;
-}
-
-.slide-fade-leave-active {
-  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
-}
-
-.slide-fade-enter-from,
-.slide-fade-leave-to {
-  transform: translateY(20px);
-  opacity: 0;
-}
 </style>
