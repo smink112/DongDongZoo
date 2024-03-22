5484c6a3b6530be5b3803447454298f4dab6e4c6
박현우 <gogoadl@naver.com>
2024-03-22 16:29:03 +0900

    feat : 책 넘기는 UI 라이브러리 및 샘플 이미지 추가

---
+++ frontend/src/components/song/StoryBook.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted } from "vue";
+
+let currentPage = 1;
+const pageNumber = 6;
+let interval: number;
+onMounted(() => {
+  $("#book").turn({
+    gradients: true,
+    acceleration: true,
+    pages: pageNumber,
+  });
+  interval = setInterval(() => {
+    $("#book").turn("page", currentPage++);
+  }, 1500);
+});
+onUnmounted(() => {
+  if (interval !== undefined) clearInterval(interval);
+});
+</script>
+
+<template>
+  <div class="container">
+    <div id="book">
+      <div>
+        <v-img src="@/assets/sample/bear1.png" />
+      </div>
+      <div>
+        <v-img src="@/assets/sample/bear2.png" />
+      </div>
+      <div>
+        <v-img src="@/assets/sample/bear3.png" />
+      </div>
+      <div>
+        <v-img src="@/assets/sample/bear4.png" />
+      </div>
+      <div>
+        <v-img src="@/assets/sample/bear5.png" />
+      </div>
+      <div>
+        <v-img src="@/assets/sample/bear6.png" />
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.container {
+  overflow: hidden;
+}
+#book {
+  margin: 5% auto;
+  width: 800px;
+  height: 400px;
+}
+
+#book .turn-page {
+  -webkit-background-size: 100% 100%;
+  background-size: 100% 100%;
+}
+
+#book div {
+  -webkit-background-size: cover;
+  background-size: cover;
+  background-position: center center;
+}
+</style>
