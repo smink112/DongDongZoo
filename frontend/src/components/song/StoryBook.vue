500decc689bd98e40cd3c32078b1b32d6747e218
박현우 <gogoadl@naver.com>
2024-03-28 10:14:25 +0900

    feat : 오디오 플레이어 및 백그라운드 이미지 추가

--- frontend/src/components/song/StoryBook.vue
+++ frontend/src/components/song/StoryBook.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted } from "vue";
+import AudioPlayer from "vue3-audio-player";
+import "vue3-audio-player/dist/style.css";
 
 let currentPage = 1;
 const pageNumber = 6;
@@ -21,37 +23,60 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="container">
-    <div id="book">
-      <div>
-        <v-img src="@/assets/sample/bear1.png" />
+  <v-col class="col" cols="12">
+    <h1 class="title">샘플 동화 읽어보기</h1>
+    <div class="container">
+      <div id="book">
+        <div class="page">
+          <v-img src="@/assets/sample/bear1.png" />
+        </div>
+        <div class="page">
+          <v-img src="@/assets/sample/bear2.png" />
+        </div>
+        <div class="page">
+          <v-img src="@/assets/sample/bear3.png" />
+        </div>
+        <div class="page">
+          <v-img src="@/assets/sample/bear4.png" />
+        </div>
+        <div class="page">
+          <v-img src="@/assets/sample/bear5.png" />
+        </div>
+        <div class="page">
+          <v-img src="@/assets/sample/bear6.png" />
+        </div>
       </div>
-      <div>
-        <v-img src="@/assets/sample/bear2.png" />
-      </div>
-      <div>
-        <v-img src="@/assets/sample/bear3.png" />
-      </div>
-      <div>
-        <v-img src="@/assets/sample/bear4.png" />
-      </div>
-      <div>
-        <v-img src="@/assets/sample/bear5.png" />
-      </div>
-      <div>
-        <v-img src="@/assets/sample/bear6.png" />
-      </div>
+      <v-col align="center" justify="center">
+        <v-col cols="8">
+          <AudioPlayer
+            :option="{
+              src: 'https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3',
+              title: '곰 세마리',
+            }"
+          />
+        </v-col>
+      </v-col>
     </div>
-  </div>
+  </v-col>
 </template>
 <style scoped>
+.col {
+  margin: 24px;
+  padding: 24px;
+  border-radius: 24px;
+  background-color: rgb(239, 239, 239);
+}
+.title {
+  margin-left: 24px;
+}
+
 .container {
   overflow: hidden;
-}
-#book {
-  margin: 5% auto;
-  width: 800px;
-  height: 400px;
+  #book {
+    margin: 5% auto;
+    width: 800px;
+    height: 400px;
+  }
 }
 
 #book .turn-page {
--- frontend/src/layouts/default/View.vue
+++ frontend/src/layouts/default/View.vue
@@ -1,7 +1,14 @@
 <script lang="ts" setup></script>
 
 <template>
-  <v-main>
+  <v-main class="background">
     <router-view> </router-view>
   </v-main>
 </template>
+
+<style scoped>
+.background {
+  background: url("../../assets/background.gif");
+  background-size: cover;
+}
+</style>
