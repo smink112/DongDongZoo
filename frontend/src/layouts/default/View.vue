b31277dc26b6c32930350eb58dd7303f5fc5ae19
박현우 <gogoadl@naver.com>
2024-03-29 13:08:04 +0900

    feat : FloatingActionButton UI 추가

--- frontend/src/layouts/default/View.vue
+++ frontend/src/layouts/default/View.vue
@@ -1,8 +1,34 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import FloatingButton from "@/components/common/FloatingButton.vue";
+import { useFloatingStore } from "@/store/floating";
+import { onMounted, onUnmounted } from "vue";
+const floatingStore = useFloatingStore();
+
+const doScroll = (event: any) => {
+  const el = event.target.documentElement;
+  const scrollTop = el.scrollTop;
+  if (scrollTop == 0) {
+    floatingStore.showFloatingButton = false;
+  } else {
+    floatingStore.showFloatingButton = true;
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", doScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", doScroll);
+});
+</script>
 
 <template>
   <v-main class="background">
     <router-view> </router-view>
+    <transition name="slide-fade">
+      <FloatingButton v-if="floatingStore.showFloatingButton" />
+    </transition>
   </v-main>
 </template>
 
