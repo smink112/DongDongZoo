4f80973e2355a3f1e685f7b4156c0d3d497298dc
Jehun Yoo <percyu98@gmail.com>
2024-03-29 13:13:28 +0900

    feat: singup & cors

--- server/src/main/java/com/dongdong/zoo/config/SecurityConfig.java
+++ server/src/main/java/com/dongdong/zoo/config/SecurityConfig.java
@@ -20,7 +20,11 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -88,5 +92,18 @@ public class SecurityConfig {
 
 	private List<String> tokenWithdrawalFilterPatterns() {
 		return List.of("/auth/logout");
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("*"));
+		configuration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
