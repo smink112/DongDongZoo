133eb120f2b81aefd18d1946b4f23780e0c9688b
sycha11 <76770475+sycha11@users.noreply.github.com>
2024-04-01 14:44:27 +0900

    Fix : 동화 생성 버그 테스트

--- frontend/src/components/story/StoryCreateBook.vue
+++ frontend/src/components/story/StoryCreateBook.vue
@@ -22,57 +22,57 @@ const pageNumber = ref(1);
 
 storyBookImageUrlList[0] = "/assets/song/bear/";
 
-songStore.createSong(
-  songId as string,
-  (res) => {
-    if (res.status == HttpStatusCode.Ok) {
-      console.log(res.data);
-      storyBookImageUrlList.value = res.data;
-    }
-  },
-  (err) => {}
-);
+// songStore.createSong(
+//   songId as string,
+//   (res) => {
+//     if (res.status == HttpStatusCode.Ok) {
+//       console.log(res.data);
+//       storyBookImageUrlList.value = res.data;
+//     }
+//   },
+//   (err) => {}
+// );
 
-// StoryBook
+// // StoryBook
 
-const getFullImageUrl = (imageUrl: string) => {
-  songDetail.value.songImageUrl = storyBookImageUrlList[0]; // 일단 생성된 이미지 url 가져옴
-  // 이미지의 경로를 조합하여 전체 이미지 URL을 반환
-  console.log(imageUrl + "0.png");
-  // 서버용 이미지
-  return imageUrl + "0.png";
-  // 로컬용 이미지
-  // return '/src' + imageUrl + '0.png';
-};
+// const getFullImageUrl = (imageUrl: string) => {
+//   songDetail.value.songImageUrl = storyBookImageUrlList[0]; // 일단 생성된 이미지 url 가져옴
+//   // 이미지의 경로를 조합하여 전체 이미지 URL을 반환
+//   console.log(imageUrl + "0.png");
+//   // 서버용 이미지
+//   return imageUrl + "0.png";
+//   // 로컬용 이미지
+//   // return '/src' + imageUrl + '0.png';
+// };
 
-watch(songDetail, () => {
-  fullImageUrl.value = getFullImageUrl(songDetail.value.songImageUrl);
-  pageNumber.value = songDetail.value.lyricsList.length;
-  console.log(pageNumber.value);
-});
+// watch(songDetail, () => {
+//   fullImageUrl.value = getFullImageUrl(songDetail.value.songImageUrl);
+//   pageNumber.value = songDetail.value.lyricsList.length;
+//   console.log(pageNumber.value);
+// });
 
-// 컴포넌트가 마운트된 후 실행되는 동작 정의
-onMounted(() => {
-  // songId를 사용하여 songDetail을 가져오고 fullImageUrl 값을 설정
-  songStore.getSong(
-    songId as string,
-    (res) => {
-      if (res.status == HttpStatusCode.Ok) {
-        console.log(res.data);
-        songDetail.value = res.data;
-      }
-    },
-    (err) => {
-      console.error(err);
-    }
-  );
-});
+// // 컴포넌트가 마운트된 후 실행되는 동작 정의
+// onMounted(() => {
+//   // songId를 사용하여 songDetail을 가져오고 fullImageUrl 값을 설정
+//   songStore.getSong(
+//     songId as string,
+//     (res) => {
+//       if (res.status == HttpStatusCode.Ok) {
+//         console.log(res.data);
+//         songDetail.value = res.data;
+//       }
+//     },
+//     (err) => {
+//       console.error(err);
+//     }
+//   );
+// });
 </script>
 <template>
   <!-- <CreateLoadingView></CreateLoadingView> -->
 
   <v-container>
-    <StoryBook :songDetail="songDetail" :pageNumber="pageNumber" />
+    <!-- <StoryBook :songDetail="songDetail" :pageNumber="pageNumber" /> -->
   </v-container>
 </template>
 
